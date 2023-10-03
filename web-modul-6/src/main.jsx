7946d674c4a58b3e247dabdafb6e6c8d3e8ec462
burjoy <luqman.hadi85@gmail.com>
2023-10-04 01:36:16 +0700

    added app.css from vite

--- web-modul-6/src/main.jsx
+++ web-modul-6/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { Test } from '../components/test.jsx'
+//import App from './App.jsx'
+import './App.css'
+// import { Test } from '../components/test.jsx'
 //import './index.css'
-import { Matrix } from '../components/matrix_a.jsx'
-import MatrixInput from '../components/matrix_a_prop.jsx'
+// import { Matrix } from '../components/matrix_a.jsx'
+// import MatrixInput from '../components/matrix_a_prop.jsx'
 import TestApp from '../components/test_app.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
