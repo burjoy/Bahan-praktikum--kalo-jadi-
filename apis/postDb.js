9b20949726a0fe7110924afc0982f74be77ecce5
burjoy <luqman.hadi85@gmail.com>
2023-10-07 10:46:03 +0700

    added api for post matrix to db and post user cred

---
+++ apis/postDb.js
@@ -0,0 +1,33 @@
+const postMatrixToDb = async (matrixA, matrixB) => {
+    try {
+      // Convert the matrixA and matrixB arrays to the format expected by the Flask server
+      const formattedMatrixA = matrixA.map(row => row.map(Number));
+      const formattedMatrixB = matrixB.map(row => row.map(Number));
+
+      // Send a POST request to the spreadsheet to set the matrices
+      const response = await fetch('http://127.0.0.1:3000/api/post_matrix', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          matrixA: formattedMatrixA,
+          matrixB: formattedMatrixB,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error setting matrices: ${response.status}`);
+      }
+
+    //   setMatrices({matrix_A: formattedMatrixA, matrix_B:formattedMatrixB});
+
+      alert('Matris berhasil disubmit!');
+      console.log([formattedMatrixA, formattedMatrixB]);
+      return "Success";
+    } catch (error) {
+      console.error('Error setting matrices:', error);
+    }
+  };
+
+export default postMatrixToDb
---
+++ apis/postUserData.js
@@ -0,0 +1,23 @@
+const postUserData =  async (npm) => {
+    try {
+        const options = {
+            method: 'POST',
+            body: JSON.stringify({
+                nomor_mahasiswa: npm,
+            }),
+            headers: {
+                'Content-Type': 'application/json', // Set the Content-Type header
+            },
+        }
+        const response = await fetch(`http://127.0.0.1:3000/login`, options);
+        console.log(response);
+        if (!response.ok) {
+            throw new Error(`Error login: ${response.status}`);
+        }
+        return response  
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default postUserData
