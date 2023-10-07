291e1b6fad74385d10f9a0aa398714340f9a6d7e
burjoy <luqman.hadi85@gmail.com>
2023-10-07 10:45:34 +0700

    added server for backend

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const sheets = google.sheets('v4');
 const { JWT } = require('google-auth-library');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 const postKomenSpreadsheet = require('./controller/postKomenSpreadsheet');
+const postToDb = require('./controller/postToDb');
+const postUser = require('./controller/postUser');
 
 const port = 3000;
 
@@ -17,56 +20,21 @@ console.log(email);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.LINK_ATLAS)
+  .then((response) => {
+    console.log("Berhasil konek ke database");
+  })
+  .catch((err) => {
+    console.log(`Error: ${err}`);
+  });
+
 app.use(cors());
 
-app.post('/api/set_matrices', postKomenSpreadsheet.postMatrixSpreadsheet);
+app.post('/api/post_matrix', postToDb.postToDb);
+
+app.post('/login', postUser.postUser);
 
 app.listen(port, () => {
     console.log(`Server berjalan di ${port}`);
 })
-
-// async function accessAndEditSpreadsheet(value) {
-//   // Authenticate with the service account
-//   const auth = new JWT({
-//     email: email,
-//     key: key,
-//     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-//   });
-
-//   // Load client secrets from a file, and set up the sheets API
-//   const sheetsAPI = google.sheets({ version: 'v4', auth });
-
-//   // Specify the spreadsheet ID and range
-//   const spreadsheetId = '1crFPc-q2unKqsKwjmTQG8xv5IieQ6irhiKQNO8LSL1A';
-//   const range = 'C5'; // Example: Edit cell A1
-
-//   // Read the current value of the cell
-//   const response = await sheetsAPI.spreadsheets.values.get({
-//     spreadsheetId,
-//     range,
-//   });
-
-//   // Update the cell with a new value
-//   const newValue = value;
-//   const updateValueCell = async() =>{
-//     try {
-//         await sheetsAPI.spreadsheets.values.update({
-//             spreadsheetId,
-//             range,
-//             valueInputOption: 'RAW',
-//             resource: {
-//               values: [[newValue]],
-//             },
-//           });
-        
-//           console.log(`Cell ${range} was updated to "${newValue}"`);
-//     } catch (error) {
-//         console.log(error);
-//     }
-    
-// }
-// updateValueCell()
-// }
-
-// accessAndEditSpreadsheet()
-//   .catch(console.error);
