ebbd555e98f8d24c7b42329b2d829690d3b6d338
burjoy <luqman.hadi85@gmail.com>
2023-10-08 07:12:47 +0700

    changed response

--- controller/postToDb.js
+++ controller/postToDb.js
@@ -2,13 +2,12 @@ const {data_mahasiswa} = require('../model/data_app');
 
 const postToDb = async(req, res) => {
     try {
-        const npm = '12345';
         //const hasil = await result.json();
-        const {matrixA, matrixB} = req.body;
-        const update = {'pre_test': matrixA, 'praktikum':matrixB};
-        const updatedDoc = await data_mahasiswa.findOneAndUpdate({'npm':npm}, update);
+        const {matrixA, matrixB, nomor_mahasiswa} = req.body;
+        const update = {'pre_test': matrixA, 'praktikum':matrixB, 'submit_pre_test':true};
+        const updatedDoc = await data_mahasiswa.findOneAndUpdate({'npm':nomor_mahasiswa}, update);
         console.log(`Updated Document: ${updatedDoc}`);
-        const result = await data_mahasiswa.findOne({'npm':npm});
+        const result = await data_mahasiswa.findOne({'npm':nomor_mahasiswa});
         console.log(`Result: ${matrixA}`);
         console.log(result);
         res.json(updatedDoc);
