9f13101f14bba9377f8efdaa83245d9dbd6930bd
burjoy <luqman.hadi85@gmail.com>
2023-10-08 21:11:56 +0700

    added scoring logic

--- controller/postToDb.js
+++ controller/postToDb.js
@@ -1,10 +1,41 @@
 const {data_mahasiswa} = require('../model/data_app');
 
+require('dotenv').config()
+
 const postToDb = async(req, res) => {
     try {
         //const hasil = await result.json();
         const {matrixA, matrixB, nomor_mahasiswa} = req.body;
-        const update = {'pre_test': matrixB, 'praktikum': matrixB, 'submit_pre_test':true};
+        const jawaban = await data_mahasiswa.findOne({'npm':process.env.NPM_ADMIN});
+        const jawabanA = jawaban.jawaban_A;
+        const jawabanB = jawaban.jawaban_B;
+        let nilai = 0;
+        for(let i = 0;i < matrixA.length;i++){
+            for(let j = 0; j < matrixA[i].length;j++){
+                if(matrixA[i][j] == jawabanA[i][j]){
+                    nilai += 8;
+                }
+                else{
+                    nilai += 4;
+                }
+            }
+        }
+
+        for(let i = 0;i < matrixB.length;i++){
+            for(let j = 0; j < matrixB[i].length;j++){
+                if(matrixB[i][j] == jawabanB[i][j]){
+                    nilai += 8;
+                }
+                else{
+                    nilai += 4;
+                }
+            }
+        }
+
+        console.log(`Nilai lu: ${nilai}`);
+
+        const update = {'pre_test': matrixA, 'praktikum': matrixB, 'submit_pre_test':true, 'nilai':nilai};
+
         const updatedDoc = await data_mahasiswa.findOneAndUpdate({'npm':nomor_mahasiswa}, update);
         console.log(`Updated Document: ${updatedDoc}`);
         const result = await data_mahasiswa.findOne({'npm':nomor_mahasiswa});
